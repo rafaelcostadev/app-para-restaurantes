200a2d6c24648cf0056c43af75c6d8ad7510a09a
Rafael Costa <rafaelcosta167@gmail.com>
2024-05-11 12:41:12 -0300

    chore: Ajuste de horario para testar das 10 até as 23h

--- js/script.js
+++ js/script.js
@@ -182,7 +182,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 22; // boolean true/false
+    return hora >= 10 && hora < 23; // boolean true/false
 
     
 }
